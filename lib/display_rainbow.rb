67dd1821f7c2c9d87ca7b488011eeb04bd6fabf3
binding-instructions-2787 <perisapeti@gmail.com>
2019-05-30 12:58:41 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,5 @@
 # Write your #display_rainbow method here
-
 def #display_rainbow(colors)
+letters = ["R","O","Y","G","B","I","V"]
+puts "#{letters[0]} #{colors[0]}, #{letters[1]} #{colors[1]}, #{letters[0]} #{colors[0]}, #{letters[0]} #{colors[0]}, #{letters[0]} #{colors[0]}, #{letters[0]} #{colors[0]}
 
-colors.each do |c|
-puts "#{letters[i]} #{c}" 
-end
