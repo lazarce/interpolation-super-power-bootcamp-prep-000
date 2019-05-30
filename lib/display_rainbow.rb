4b26f73aec44aa29a51faeba3efd3a9f349d6977
binding-instructions-2787 <perisapeti@gmail.com>
2019-05-30 13:09:47 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
 
-def #display_rainbow(colors)
+def #display_rainbow(colors=[])
 
 puts 'R: #{colors[0]}, O: #{colors[1]}, G:  #{colors[2]}, Y: #{colors[3]}, I: #{colors[4]}, B: #{colors[5]}, V: #{colors[6]}'
 end
